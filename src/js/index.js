5b3a827e3a05e56ccf0a5032735f4aae098fe5f5
Zhenjun <zzuwzj@163.com>
2015-11-18 22:20:46 +0800

    Implement index.html, add unslider

---
+++ src/js/index.js
@@ -0,0 +1,10 @@
+$(function() {
+    var unslider = $('.banner').unslider();
+
+    $('.unslider-arrow').click(function() {
+        var fn = this.className.split(' ')[1];
+
+        //  Either do unslider.data('unslider').next() or .prev() depending on the className
+        unslider.data('unslider')[fn]();
+    });
+});
